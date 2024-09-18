589022a9d6cae18628a9c60d0dcb25ecce665785
Isadora <isadora.c.lopes@gmail.com>
2024-09-18 15:06:49 -0300

    Adding logic

--- app/src/main/java/com/isadoralopes/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/isadoralopes/bmicalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.isadoralopes.bmicalculator
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalculate: Button
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +26,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnCalculate = findViewById(R.id.button_calculate)
+        editWeight = findViewById(R.id.edit_weight)
+        editHeight = findViewById(R.id.edit_height)
 
         btnCalculate.setOnClickListener {
 
             val intent = Intent(this, ResultActivity::class.java)
+
+            val weight = editWeight.text.toString()
+            val height = editHeight.text.toString()
+
+            if ( weight.isNotEmpty() && height.isNotEmpty() ) {
+
+                intent.putExtra("weight", weight.toDouble() )
+                intent.putExtra("height", height.toDouble() )
+            }
 
             startActivity( intent )
         }
