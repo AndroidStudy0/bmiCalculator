c51c5d18a7259422a7852681e96e43613c679a3f
Isadora <isadora.c.lopes@gmail.com>
2024-09-18 15:06:49 -0300

    Adding logic

--- app/src/main/java/com/isadoralopes/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/isadoralopes/bmicalculator/ResultActivity.kt
@@ -1,12 +1,18 @@
 package com.isadoralopes.bmicalculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
+
+    private  lateinit var textWeight: TextView
+    private  lateinit var textHeight: TextView
+    private  lateinit var textResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,35 @@ class ResultActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        textWeight = findViewById(R.id.text_weight)
+        textHeight = findViewById(R.id.text_height)
+        textResult = findViewById(R.id.text_result)
+
+        val bundle = intent.extras
+
+        if ( bundle != null ) {
+
+            val weight = bundle.getDouble("weight")
+            val height = bundle.getDouble("height")
+
+            textWeight.text = "Reported weight: $weight kg"
+            textHeight.text = "Reported height: $height m"
+
+            val bmi = weight / ( height * height )
+
+            val result = if ( bmi < 18.5 ) {
+                "Low"
+            } else if (bmi in 18.5..24.9) {
+               "Normal"
+            } else if (bmi in 25.0..29.9) {
+               "Overweight"
+            } else {
+               "Obese"
+            }
+
+            textResult.text = result
         }
     }
 }
