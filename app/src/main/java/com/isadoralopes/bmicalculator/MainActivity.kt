743b16d0160f7f94c781d8264df4e704e5e0bdcc
Isadora <isadora.c.lopes@gmail.com>
2024-09-18 14:20:41 -0300

    Creating layout

---
+++ app/src/main/java/com/isadoralopes/bmicalculator/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.isadoralopes.bmicalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalculate: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnCalculate = findViewById(R.id.button_calculate)
+
+        btnCalculate.setOnClickListener {
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            startActivity( intent )
+        }
+    }
+}
